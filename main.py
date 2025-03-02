191fcfeb2ebb67b2e8dca8e1bc4e53758b3d76c9
Mariia <mariia.samoilenko.06@gmail.com>
2025-03-02 23:01:43 +0200

    Revert "add interactive mode": Insufficient error handling causing crashes
    This reverts commit 3b3d5eaff0cfa4ccdfda6731c8e83121d773034f.

--- main.py
+++ main.py
@@ -9,23 +9,3 @@ def getQuadraticRoots(a, b, c):
         return (x1, x2) if x1 != x2 else (x1,)
     else:
         return None
-
-def interactive_mode():
-    try:
-        a = float(input("Enter non-zero a: "))
-        b = float(input("Enter b: "))
-        c = float(input("Enter c: "))
-    except ValueError as err:
-        print(f"Error: {err}")
-        return
-
-    print(f"Quadratic equation: {a}x² + {b}x + {c} = 0")
-    roots = getQuadraticRoots(a, b, c)
-
-    if roots is None:
-        print("No real roots")
-    else:
-        print("The roots are:", ", ".join(map(str, roots)))
-
-if __name__ == "__main__":
-    interactive_mode()
